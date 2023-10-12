255e323118253ded92d6c269efbfc500204bbf27
Murashkin Sergey <workbox_55@mail.ru>
2023-10-12 17:56:43 +0600

    init commit

---
+++ src/main/java/ru/murashkin/tutorial/spring/cloud/SpringCloudTutorialApplication.java
@@ -0,0 +1,13 @@
+package ru.murashkin.tutorial.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/murashkin/tutorial/spring/cloud/SpringCloudTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.murashkin.tutorial.spring.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
